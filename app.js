1b389e168b0aabfda73e02777d147510c2b091c0
Gladson Dutra <gladsonddutra@gmail.com>
2023-12-28 15:21:26 -0300

    First commit

---
+++ app.js
@@ -0,0 +1,32 @@
+const express = require('express')
+const app = express()
+
+app.use(express.static('public'))
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/public/index.html')
+})
+
+const result = { "unix": "", "utc": "" }
+
+app.get('/api/:date?', (req, res) => {
+  const input = req.params.date || Date.now();
+  const date = new Date(input);
+
+  if (!isNaN(date)) {
+    result.unix = Math.floor(date / 1000);
+    result.utc = date.toString();
+    res.json(result);
+  }
+  else if (new Date(input * 1000) != "Invalid Date") {
+    result.unix = parseInt(input)
+    result.utc = new Date(input * 1000).toString()
+    res.json(result)
+  } else {
+    res.json({ error: "Invalid Date" });
+  }
+});
+
+app.listen(3000, () => {
+  console.log('App listening on the port 3000')
+})
