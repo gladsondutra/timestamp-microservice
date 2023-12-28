70bd1c55f7f09720e403cea2bacc545cdfa54d85
Gladson Dutra <gladsonddutra@gmail.com>
2023-12-28 15:57:45 -0300

    Express port updated

--- index.js
+++ index.js
@@ -27,6 +27,7 @@ app.get('/api/:date?', (req, res) => {
   }
 });
 
-app.listen(3000, () => {
-  console.log('App listening on the port 3000')
+const port = process.env.PORT || 3000
+app.listen(port, () => {
+  console.log(`App listening on the port ${port}`)
 })
